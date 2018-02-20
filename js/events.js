9057df11b197f86ee614a72c15027a976884618f
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-20 13:38:01 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function frameIt(){
 function pressIt(){
   $(input).on('keydown', function(key) {
     if(key.which == 71){
-      alert('s was pushed');
+      alert('G was pushed');
     }
   });
 }
